0f914443a308ad9217b2e098710989ebe225ebc3
Elliott <elliottcost@gmail.com>
2019-12-02 19:10:56 -0500

    clean up

--- app.js
+++ app.js
@@ -6,23 +6,11 @@ const router = express.Router();
 app.use(bodyParser.json());
 const path = require('path');
 const Arena = require('are.na');
-const { check, validationResult } = require('express-validator');
 let arena = new Arena({ accessToken: process.env.ARENA_API_KEY });
-
-const db = require("./db");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/getid/:channel', function(req, res){
-  arena.channel(req.params.channel).get()
-  .then(chan => {
-      // console.log(chan);
-      res.send({"id" : chan.id});
-  })
-  .catch(err => console.log(err));
-})
-
-app.get('/sites', function(req, res){
+app.get('/api/sites', function(req, res){
   arena.channel('scoby').get()
   .then(chan => {
       res.send(chan);
@@ -52,14 +40,6 @@ app.use(express.static(__dirname + '/public', {
     extensions: ['html', 'htm'],
 }));
 
-db.connect((err)=>{
-  if(err){
-      console.log('unable to connect to database');
-      process.exit(1);
-  }
-  else{
-      app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', ()=>{
-          console.log('connected to database, app listening on port 3000')
-      });
-  }
-})
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', ()=>{
+  console.log('connected to database, app listening on port 3000')
+});
