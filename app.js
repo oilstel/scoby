7d73e957dd4eb99588c297b5e2de0597e4971b74
Elliott Cost <email@elliott.computer>
2025-04-19 01:18:32 +0200

    fixes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const router = express.Router();
 app.use(bodyParser.json());
 const path = require('path');
+const Arena = require('are.na');
+let arena = new Arena();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -67,76 +69,24 @@ app.get('/api/sites', async function(req, res) {
   }
 });
 
-app.get('/api/:site', async function(req, res) {
-    console.log(req.params.site);
-    const response = await fetch(`https://api.are.na/v2/channels/${req.params.site}/contents?per=24&page=1&direction=desc&sort=position`, {
-        method: 'GET',
-        headers: {
-            'Cache-Control': 'no-cache'
-        }
-    });
-
-    const data = await response.json();
-    res.send(data);
-});
-
-// app.get('/api/:site', async function(req, res) {
-    
-//   const page = req.query.page || 1;
-//   const per = req.query.per || 24;
-  
-//   try {
-//     const response = await fetch(`https://api.are.na/v2/channels/${req.params.site}/contents?per=${per}&page=${page}&direction=desc&sort=position`, {
-//       method: 'GET',
-//       headers: {
-//         'Cache-Control': 'no-cache'
-//       }
-//     });
-    
-//     if (!response.ok) {
-//       const errorText = await response.text();
-//       console.error('API Error:', {
-//         status: response.status,
-//         statusText: response.statusText,
-//         headers: Object.fromEntries(response.headers.entries()),
-//         body: errorText
-//       });
-//       res.status(response.status).send({
-//         error: 'API request failed',
-//         status: response.status,
-//         message: errorText
-//       });
-//       return;
-//     }
-
-//     const data = await response.json();
-//     console.log('API Response for /api/:site:', JSON.stringify(data).slice(0, 200));
-    
-//     // Handle different response formats
-//     let contents = [];
-//     if (data.contents) {
-//       contents = data.contents;
-//     } else if (Array.isArray(data)) {
-//       contents = data;
-//     }
-    
-//     // Add pagination info to the response
-//     const responseData = {
-//       contents: contents,
-//       current_page: parseInt(page),
-//       per_page: per,
-//       has_more: contents.length === parseInt(per)
-//     };
-    
-//     res.send(responseData);
-//   } catch (err) {
-//     console.error('Fetch Error:', err);
-//     res.status(500).send({
-//       error: 'Internal server error',
-//       message: err.message
-//     });
-//   }
-// });
+app.get('/api/:site', function(req, res){
+    // add search params for page, eg /api/scoby?page=2
+    // ------------------------------------------------
+    // its an art to find the perfect balance between
+    // amount of blocks to return per request and the
+    // actual amount of requests needed to load content
+    arena.channel(req.params.site).contents({ page: req.query.page ? req.query.page : 1, 
+      per: req.query.per ? req.query.per : 24,
+      direction: 'desc',
+      sort: 'position'
+     })
+      .then(contents => {
+        // console.log(contents)
+        res.send(contents);
+        // res.sendFile(__dirname + '/public/index.html');
+      })
+      .catch(err => console.log(err));
+  })
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
